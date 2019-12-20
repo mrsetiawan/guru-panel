6377d1a2caeea2baca1df9de5230630bfdaffbfd
agungjati <agungjati94@gmail.com>
2019-12-20 17:31:17 +0700

    integrasi admin lte dan next

--- pages/index.js
+++ pages/index.js
@@ -1,88 +1,166 @@
-import React from 'react'
-import Head from 'next/head'
-import Nav from '../components/nav'
+import React from 'react';
+import Head from 'next/head';
+import Navbar from '../components/Navbar';
 
 const Home = () => (
-  <div>
+ <div>
     <Head>
       <title>Home - React NextJS</title>
       <link rel="icon" href="/favicon.ico" />
+      <link rel="stylesheet" href="/plugins/fontawesome-free/css/all.min.css" />
+      <link rel="stylesheet" href="/dist/css/adminlte.min.css" />
+      <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet" />
     </Head>
+  
+<div className="wrapper">
+  <Navbar />
+  <aside className="main-sidebar sidebar-dark-primary elevation-4">
+    <a href="index3.html" className="brand-link">
+      <img src="/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" className="brand-image img-circle elevation-3"
+            />
+      <span className="brand-text font-weight-light">AdminLTE 3</span>
+    </a>
+    <div className="sidebar">
+      <div className="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div className="image">
+          <img src="/dist/img/user2-160x160.jpg" className="img-circle elevation-2" alt="User Image" />
+        </div>
+        <div className="info">
+          <a href="#" className="d-block">Alexander Pierce</a>
+        </div>
+      </div>
+      <nav className="mt-2">
+        <ul className="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <li className="nav-item has-treeview menu-open">
+            <a href="#" className="nav-link active">
+              <i className="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Starter Pages
+                <i className="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul className="nav nav-treeview">
+              <li className="nav-item">
+                <a href="#" className="nav-link active">
+                  <i className="far fa-circle nav-icon"></i>
+                  <p>Active Page</p>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#" className="nav-link">
+                  <i className="far fa-circle nav-icon"></i>
+                  <p>Inactive Page</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-link">
+              <i className="nav-icon fas fa-th"></i>
+              <p>
+                Simple Link
+                <span className="right badge badge-danger">New</span>
+              </p>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </aside>
 
-    <Nav />
-
-    <div className="hero">
-      <h1 className="title">Welcome to Next.js!</h1>
-      <p className="description">
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
-
-      <div className="row">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Learn more about Next.js in the documentation.</p>
-        </a>
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Next.js Learn &rarr;</h3>
-          <p>Learn about Next.js by following an interactive tutorial!</p>
-        </a>
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Find other example boilerplates on the Next.js GitHub.</p>
-        </a>
+  <div className="content-wrapper">
+    <div className="content-header">
+      <div className="container-fluid">
+        <div className="row mb-2">
+          <div className="col-sm-6">
+            <h1 className="m-0 text-dark">Starter Page</h1>
+          </div>
+          <div className="col-sm-6">
+            <ol className="breadcrumb float-sm-right">
+              <li className="breadcrumb-item"><a href="#">Home</a></li>
+              <li className="breadcrumb-item active">Starter Page</li>
+            </ol>
+          </div>
+        </div>
       </div>
     </div>
+    <div className="content">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-lg-6">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
 
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up the bulk of the card's
+                  content.
+                </p>
+
+                <a href="#" className="card-link">Card link</a>
+                <a href="#" className="card-link">Another link</a>
+              </div>
+            </div>
+
+            <div className="card card-primary card-outline">
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up the bulk of the card's
+                  content.
+                </p>
+                <a href="#" className="card-link">Card link</a>
+                <a href="#" className="card-link">Another link</a>
+              </div>
+            </div>
+          </div>
+         
+          <div className="col-lg-6">
+            <div className="card">
+              <div className="card-header">
+                <h5 className="m-0">Featured</h5>
+              </div>
+              <div className="card-body">
+                <h6 className="card-title">Special title treatment</h6>
+
+                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+
+            <div className="card card-primary card-outline">
+              <div className="card-header">
+                <h5 className="m-0">Featured</h5>
+              </div>
+              <div className="card-body">
+                <h6 className="card-title">Special title treatment</h6>
+
+                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-)
+  <aside className="control-sidebar control-sidebar-dark">
+    <div className="p-3">
+      <h5>Title</h5>
+      <p>Sidebar content</p>
+    </div>
+  </aside>
+  <footer className="main-footer">
+    <div className="float-right d-none d-sm-inline">
+      Anything you want
+    </div>
+    <strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+  </footer>
+</div>
+<script src="/plugins/jquery/jquery.min.js"></script>
+<script src="/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="/dist/js/adminlte.min.js"></script>
+</div>)
 
 export default Home

e8b2502ed168c94384b9c3762f6d46d2b33ed7af
Suherman <suherman@qantas.com.au>
2020-01-03 06:50:09 +0700

    Resolve conflict from Agung Jati.

--- pages/script-tag.js
+++ pages/script-tag.js
@@ -5,6 +5,7 @@ const ScriptTag = (props) => (
       <script src="/plugins/jquery/jquery.min.js"></script>
       <script src="/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
       <script src="/dist/js/adminlte.min.js"></script>
+      <script src="https://cdn.ckeditor.com/4.13.1/standard-all/ckeditor.js"></script>
       {props.children}
     </>
 )
